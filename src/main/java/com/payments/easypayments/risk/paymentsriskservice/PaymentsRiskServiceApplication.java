da579cec4f20cfb3b10bea561029f03a52a2cc5b
Amit Chandra <amchandra@paypal.com>
2022-05-08 11:34:00 +0530

    INitial commit

---
+++ src/main/java/com/payments/easypayments/risk/paymentsriskservice/PaymentsRiskServiceApplication.java
@@ -0,0 +1,13 @@
+package com.payments.easypayments.risk.paymentsriskservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentsRiskServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentsRiskServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/payments/easypayments/risk/paymentsriskservice/PaymentsriskserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.payments.easypayments.risk.paymentsriskservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentsriskserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
